7c9c80b304c240e5982f0d22577f643b237a3492
psyeb6 <psyeb6@nottingham.ac.uk>
2023-11-07 21:12:56 +0000

    Setup google authentication and connection to backend and create basic blank home activity

--- app/src/main/java/com/example/group10_sqa_mentalhealthapp/HomeActivity.java
+++ app/src/main/java/com/example/group10_sqa_mentalhealthapp/HomeActivity.java
@@ -3,12 +3,14 @@ package com.example.group10_sqa_mentalhealthapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+        Log.d("Main", "Home Activity Launched");
     }
 }
