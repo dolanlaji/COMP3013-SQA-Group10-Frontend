424f9d029520c8f7a4ca4eb47706cccbab07dc33
Daniel Valadkevich <danielvaladkevich@gmail.com>
2023-11-26 21:05:09 +0000

    Hooked button to send back RESULT_OK

--- app/src/main/java/com/example/group10_sqa_mentalhealthapp/DisclaimerActivity.java
+++ app/src/main/java/com/example/group10_sqa_mentalhealthapp/DisclaimerActivity.java
@@ -2,7 +2,9 @@ package com.example.group10_sqa_mentalhealthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DisclaimerActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DisclaimerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disclaimer);
+    }
+
+    public void onAgree(View v) {
+        setResult(Activity.RESULT_OK);
+        finish();
     }
 }
